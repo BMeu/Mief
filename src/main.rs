ab3b08e79e1b7e3b5ec06564e44a0d8e55ab4b03
BMeu <bastian@bastianmeyer.eu>
2017-05-16 21:18:42 +0200

    Set up Rust project

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+// Copyright 2017 Bastian Meyer
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or http://apache.org/licenses/LICENSE-2.0> or the
+// MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option. This file may not be copied,
+// modified, or distributed except according to those terms.
+
+//!
+
+#![warn(missing_docs,
+        missing_debug_implementations, missing_copy_implementations,
+        trivial_casts, trivial_numeric_casts,
+        unused_extern_crates, unused_import_braces, unused_qualifications, unused_results)]
+#![cfg_attr(feature = "cargo-clippy", warn(empty_enum, enum_glob_use, if_not_else, items_after_statements,
+                                           missing_docs_in_private_items, nonminimal_bool,
+                                           pub_enum_variant_names, similar_names, single_match_else,
+                                           stutter, used_underscore_binding, use_debug, wrong_self_convention,
+                                           wrong_pub_self_convention))]
+
+fn main() {
+    println!("Hello, world!");
+}
