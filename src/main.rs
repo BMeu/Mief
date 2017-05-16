6e8aca6b79c70a46776e17a8c3c4969c9c727df8
BMeu <bastian@bastianmeyer.eu>
2017-05-16 21:34:10 +0200

    Add documentation

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 // MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option. This file may not be copied,
 // modified, or distributed except according to those terms.
 
-//!
+//! This is the main module for `Mief`.
 
 #![warn(missing_docs,
         missing_debug_implementations, missing_copy_implementations,
