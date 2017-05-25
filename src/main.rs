bd6572ea8ae6c659219d27d98d9a9f2547d64b2b
BMeu <bastian@bastianmeyer.eu>
2017-05-25 19:44:06 +0200

    Add more clippy lints

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,9 @@
         missing_debug_implementations, missing_copy_implementations,
         trivial_casts, trivial_numeric_casts,
         unused_extern_crates, unused_import_braces, unused_qualifications, unused_results)]
-#![cfg_attr(feature = "cargo-clippy", warn(empty_enum, enum_glob_use, if_not_else, items_after_statements,
-                                           missing_docs_in_private_items, nonminimal_bool,
+#![cfg_attr(feature = "cargo-clippy", warn(cast_possible_truncation, cast_possible_wrap, cast_precision_loss,
+                                           cast_sign_loss, empty_enum, enum_glob_use, if_not_else,
+                                           items_after_statements, missing_docs_in_private_items, nonminimal_bool,
                                            pub_enum_variant_names, similar_names, single_match_else,
                                            stutter, used_underscore_binding, use_debug, wrong_self_convention,
                                            wrong_pub_self_convention))]
